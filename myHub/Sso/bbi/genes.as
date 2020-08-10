c39ff77426cda92cb771bd0217619b516bb4e16d
dmatelska <dmatelska@genesilico.pl>
2020-08-10 18:27:53 +0200

    First commit

---
+++ myHub/Sso/bbi/genes.as
@@ -0,0 +1,14 @@
+table genes
+"Anotations of the Sso genome"
+(
+string  chrom;		"Reference sequence chromosome or scaffold"
+uint    chromStart;	"Start position of feature on chromosome"
+uint    chromEnd;	"End position of feature on chromosome"
+string  name;		"Name of gene"
+uint    score;		"Score"
+char[1] strand;		"+ or - for strand"
+uint    thickStart;	"Coding region start"
+uint    thickEnd;	"Coding region end"
+uint  	reserved;	"Color of the feature"
+string  geneProduct;	"Gene product description"
+)
---
+++ myHub/Sso/bbi/make_bigBed.py
@@ -0,0 +1,37 @@
+import os
+
+gff = 'genes.gff3'
+out = 'genes.bb'
+
+colors = ['44,160,44','31,119,180','255,127,14','214,39,40','31,119,180']
+biotype2color = {}
+
+with open(out, 'w') as f:
+	for line in open(gff):
+		tab = line.strip().split('\t')
+		if len(tab) < 8:
+			continue
+		desc = tab[8]
+		extra = desc
+		if tab[2] == 'gene':
+			if 'old_locus_tag=' in desc:
+				xid = desc.split('old_locus_tag=')[1].strip()
+			else:
+				xid = desc.split('ID=')[1].split(';')[0]
+			biotype = 'protein_coding'
+			if 'gene_biotype=' in desc:
+				biotype = desc.split('gene_biotype=')[1].split(';')[0]
+			if biotype in biotype2color:
+				col = biotype2color[biotype]
+			else:
+				col = colors.pop()
+				biotype2color[biotype] = col
+
+			print(tab[0], tab[3], tab[4], xid, 1000, tab[6], tab[3], tab[4], col, extra, sep='\t', file=f)
+
+
+os.system('sort -k1,1 -k2,2n %s > tmp2' % out)
+#os.system('echo "track name=Genes type=bedDetail" > tmp1')
+#os.system('cat tmp1 tmp2 > tmp3')
+os.system('bedToBigBed -as=genes.as -extraIndex=name -type=bed9+1 -tab tmp2 chrom.sizes %s' % out)
+os.system('rm tmp2')
